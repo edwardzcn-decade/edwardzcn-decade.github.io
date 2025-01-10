4f3c7b819ea0c638b45389e70e20562dda086a79
edwardzcn-decade <edwardzcn-decade@users.noreply.github.com>
2025-01-10 13:53:46 +0000

    deploy: edwardzcn-decade/new-blog-source@91bb38b732efb854ca0944afa65b37e6a0ecb3db

--- js/toggle-sections.js
+++ js/toggle-sections.js
@@ -19,3 +19,44 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const searchBox = document.getElementById("searchbox");
+  const searchModal = document.querySelector("#searchbox .search-modal");
+  const searchInput = document.getElementById("searchInput");
+  const searchButton = document.getElementById("searchButton");
+  const searchLink = document.querySelector('a[title="Search"]');
+
+  searchLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    searchBox.classList.remove("hidden");
+    searchInput.focus(); // focus on input
+  });
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      searchBox.classList.add("hidden");
+    }
+  });
+
+  document.addEventListener("click", function (event) {
+    // check if click inside the search panel
+    const isClickInside =
+      searchModal.contains(event.target) || searchLink.contains(event.target);
+    if (!isClickInside) {
+      searchBox.classList.add("hidden");
+    }
+  });
+
+  // TODO search function
+  searchButton.addEventListener("click", function () {
+    const searchTerm = searchInput.value.trim();
+
+    if (searchTerm) {
+      // TODO jump to search page?
+      const searchUrl = `/search.html?q=${encodeURIComponent(searchTerm)}`;
+      window.location.href = searchUrl;
+    } else {
+      alert("Please enter a search term.");
+    }
+  });
+});
